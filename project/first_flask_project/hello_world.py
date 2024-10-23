f28930f77ea2c41b3a0f63707360985031f48d56
Mustafa Aktepe <musttafaaktepe@gmail.com>
2024-10-23 22:45:09 +0300

    04

--- project/first_flask_project/hello_world.py
+++ project/first_flask_project/hello_world.py
@@ -19,5 +19,5 @@ def fourth(id):
 
 if __name__ == '__main__':
 
-     app.run(debug=True)
-     app.run(host= '0.0.0.0', port=8080)
+     #app.run(debug=True)
+     app.run(host= '0.0.0.0', port=80)
